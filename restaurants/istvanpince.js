61a7819138870cab59eff9f664db0d0ce59ccb4b
Balint Kovacs <balint@y-collective.hu>
2017-10-19 23:58:12 +0200

    fix István pince menü display
    refs #2

--- restaurants/istvanpince.js
+++ restaurants/istvanpince.js
@@ -7,15 +7,18 @@ module.exports = {
 	url: 'https://www.facebook.com/istvanpince/',
 	getMenu: body => {
 		const $ = cheerio.load(body);
-		const separators = ['<br>', '<p>'];
+		const separators = ['<br>', '<p>', '</span>'];
 		const dailyMenu = $('.userContent').filter((i, e) => {
 			return e.children.length;
 		}).eq(0).find('.text_exposed_root').html();
 
 		return dailyMenu.split(new RegExp(separators.join('|'), 'g'))
+			.filter(function(text) {
+				return text.startsWith(' ');
+			})
 			.map(el => el.trim())
 			.map(el => striptags(el))
 			.map(el => el.replace(/\.\.\./g, ''))
 			.map(el => he.decode(el));
-	}
+		}
 };
