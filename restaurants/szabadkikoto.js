9d5f5427f3e250c8532b2b2327fc013bcb6c4cff
ocReaper <ocreaper@gmail.com>
2019-09-10 10:20:22 +0200

    add szabadkikötő, but statically

---
+++ restaurants/szabadkikoto.js
@@ -0,0 +1,35 @@
+const cheerio = require('cheerio');
+const he = require('he');
+const striptags = require('striptags');
+
+module.exports = {
+	name: 'Szabadkikötő',
+	url: 'https://www.facebook.com/SzabadKikotoPecs',
+	getMenu: (body, date = new Date()) => {
+		// try {
+		// 	const $ = cheerio.load(body);
+		// 	const separators = ['<br>', '<p>', '</div>'];
+		// 	const dailyMenu = $('.userContent').filter((i, e) => {
+		// 		return e.children.length;
+		// 	}).eq(0).html();
+		//
+		// 	return dailyMenu
+		// 		.split(new RegExp(separators.join('|'), 'g'))
+		// 		.map(el => striptags(el))
+		// 		.map(el => el.trim())
+		// 		.map(el => el.replace(/\.\.\./g, ''))
+		// 		.map(el => he.decode(el));
+		// } catch (e) {
+		// 	return 'Hiba történt a begyűjtés során..';
+		// }
+		const hetimenu = [
+			'párolt zöldséges subji, jázmin rizzsel és paradicsom chatnival',
+			'lencsefőzelék, répafasirttal',
+			'chilli sin carne, házi lepénykenyérrel',
+			'paradicsomos-cukkinis penne',
+			'tofus spenót, kukoricás jázmin rizzsel',
+		];
+
+		return hetimenu[date.getDay() -1];
+	}
+};
