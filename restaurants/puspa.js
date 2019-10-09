9a1dc7c0874d3d11c279f35fa7f11f992943fc80
ocReaper <ocreaper@gmail.com>
2019-10-09 19:17:22 +0200

    remove date from output string

--- restaurants/puspa.js
+++ restaurants/puspa.js
@@ -18,6 +18,7 @@ module.exports = {
 				.map(el => el.trim())
 				.map(el => striptags(el))
 				.map(el => el.replace(/[\n\t]/g, ''))
+				.map(el => el.replace((date.getMonth() + 1) + '.' + (date.getDate() - date.getDay() + 1) + '-' + (date.getDate() - date.getDay() + 5), ''))
 				.map(el => he.decode(el));
 		} catch (err) {
 			return 'Jelenleg nem elérhető a menü...';
