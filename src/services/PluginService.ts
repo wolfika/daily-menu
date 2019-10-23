f5f6abd04f9f481f1125438d18b655bc492cc1ed
Mate Farkas <mate.farkas@logmein.com>
2019-10-23 23:21:16 +0200

    implement PluginService#loadAll() async, make it async

--- src/services/PluginService.ts
+++ src/services/PluginService.ts
@@ -1,9 +1,11 @@
 import { PluginService as PluginServiceAbstraction } from '../abstractions/PluginService';
 import { ConfigService } from './ConfigService';
 import { INSTALLED_PLUGINS_KEY } from '../constants';
+import { Plugin } from '../abstractions/Plugin';
 
 export class PluginService implements PluginServiceAbstraction {
   private pluginPaths: string[];
+  private plugins: Plugin[];
 
   constructor(private readonly configService: ConfigService) {
     this.pluginPaths = this.configService.get(INSTALLED_PLUGINS_KEY, []);
@@ -17,7 +19,9 @@ export class PluginService implements PluginServiceAbstraction {
     return;
   }
 
-  loadAll(): void {
-    return;
+  async loadAll(): Promise<void> {
+    const pluginModulePromises = this.pluginPaths.map((pluginPath: string): Promise<any> => import(pluginPath));
+    const PluginConstructors = (await Promise.all(pluginModulePromises)).map((pluginModule) => pluginModule.default);
+    this.plugins = PluginConstructors.map((PluginConstructor: any) => new PluginConstructor());
   }
 }
